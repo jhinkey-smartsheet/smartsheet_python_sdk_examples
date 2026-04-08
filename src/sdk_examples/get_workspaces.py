b8683856a9cabb88dd358eeeea4a22af7e8f058a
Jim Hinkey <yojimmyh@gmail.com>
2026-04-08 13:47:14 -0400

    Add get_workspaces module

---
+++ src/sdk_examples/get_workspaces.py
@@ -0,0 +1,30 @@
+import argparse
+import os
+
+import smartsheet
+
+def main() -> None:
+    token = os.getenv("SMARTSHEET_API_TOKEN")
+    print(f"SMARTSHEET_API_TOKEN set: {bool(token)}")
+    if not token:
+        raise RuntimeError("Missing required environment variable: SMARTSHEET_API_TOKEN")
+
+    smart = smartsheet.Smartsheet(token)
+
+    last_key = None
+    while last_key != "":
+        page = smart.Workspaces.list_workspaces(
+            pagination_type='token',
+            last_key=last_key
+        )
+        assert isinstance(page, smartsheet.models.IndexResult)
+
+        print(page.to_dict()) # Do what you want with the page items
+
+        last_key = getattr(page, "last_key", None)
+        if not last_key:
+            break
+
+
+if __name__ == "__main__":
+    main()
